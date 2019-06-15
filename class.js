626aa462fc833ae17465ab0bba0e973ea13a2a0f
Teezolusegun <teezolusegun@gmail.com>
2019-06-15 21:39:35 +0100

    working with chibike

---
+++ class.js
@@ -0,0 +1,81 @@
+// let names = ["goke", "emmanuel", "jumia", "cynthia", "father"];
+// names.forEach(function(name){
+//   console.log(  `${names +="smith"}`)
+// }
+// )
+// let numbers =[1, 2, 3, 4, 5];
+// let double = numbers.map((onebyone) =>{
+// return onebyone * 2
+// } )
+// console.log(double)
+
+// let jambScore = [180, 125, 250, 270, 141 ]
+// let passScore = jambScore.filter((each) => {
+//     return each >= 180
+// })
+// console.log(passScore)
+
+// const name =  user = {
+// name : "Emmanuel",
+// age : 27,
+// occupation : "student",
+// social : {
+//     fb: "facebook something",
+//     tiq : "tiq something"
+// }
+
+
+// }
+// const {fb} = user.social
+// console.log(fb);
+
+// let arr = ["hey", "where", "are"]
+// let [first, second , third] = arr
+// console.log(first)
+
+// const str = ["hello", "world"]
+// console.log(...str)
+
+// const teamA = ["Olivia", "Emmanuel", "Banabas", "Dennis"];
+// const teamB = ["Engee", "Japhet", "Chibike", "Ubbana"];
+//  const allTeams = [...teamA, ...teamB]
+//  console.log(allTeams)
+
+// function convertCurrency(rate, ...amounts){
+//     return amounts.map(amount => amount * rate);
+// }
+// const amounts = convertCurrency(1.54 , 10, 20, 30, 25)
+// console.log(amounts);
+//assignment 1
+//  let array = [180, 125, 250 , 270, 141]
+//  function sumArray(all, num){
+//      return all + num ;
+//  }
+//  let summOfAll = array.reduce(sumArray);
+//  console.log(summOfAll);
+
+
+//  //assignment 2
+//  const user = [
+//      {
+//          name : "favour",
+//          age : 30,
+//          isworking : true
+//      },
+//      {
+//          name : "Olivia",
+//          age : 35 ,
+//          isworking : true
+//      },
+//      {
+//          name : "Ubanna",
+//          age : 28 , 
+//          isworking : false
+//      }
+
+//  ]
+
+//  let returnArray = user.filter((each) => {
+//      return each.age > 30 && each.isworking == true;
+//  })
+//  console.log(returnArray)
